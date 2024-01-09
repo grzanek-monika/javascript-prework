936651094ce6fcd6f6ee4fec448cf94fe8072a79
Monika Grzanek <grzanekmonika@o2.pl>
2024-01-09 23:52:38 +0100

    Add js/function.js and add basic code in html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
