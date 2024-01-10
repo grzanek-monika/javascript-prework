8d0069d7a36c5a45d85683ff7e3a7b41d4b47f04
Monika Grzanek <grzanekmonika@o2.pl>
2024-01-10 07:48:12 +0100

    Add let computerMove and let playerMove and instruction if else in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log(`Wylosowana liczba to: ${randomNumber}`);
+
+let computerMove = 'nieznana liczba';
+
+if(randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier'
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce'
+} 
+
+printMessage(`Mój ruch to ${computerMove}`);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+let playerMove = 'nienzany  ruch';
+
+if(playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if(playerInput == '3') {
+    playerMove = 'nożyce'
+}
+
+printMessage(`Twój ruch to: ${playerMove}`);
+
+/*printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
 
 let computerMove = `kamień`;
 
@@ -6,4 +35,4 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 
 let playerMove = 'papier';
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');*/
